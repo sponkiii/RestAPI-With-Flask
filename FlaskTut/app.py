2a1c9d9ca80795cfd9f85701f1026c9d18813b47
Arvin Ramirez <arvinken07@yahoo.com>
2021-02-18 22:15:58 +0800

    first commit

---
+++ FlaskTut/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello boi World<h1>'
+
+
+# another route
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi {}, musta ka jan pre? kaen muna.'.format(name)
+
+
+# for auto restart every changes you make
+if __name__ == '_main_':
+    app.run(debug=True)
